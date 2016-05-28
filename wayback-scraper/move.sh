7a50eab6946074df1add2b574d5b2e7b8e4f6620
Ryan Albrecht <ryan@ryanalbrecht.ca>
2016-05-28 10:25:46 -0700

    Re-import files from the scraper with the correct extension this time.

--- wayback-scraper/move.sh
+++ wayback-scraper/move.sh
@@ -7,7 +7,7 @@ files=`find ./data -type f | grep "[0-9]/"`
 
 for file in $files;
 do
-  date=`echo "$file" | sed -E 's/.*\/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{6}).*/\1-\2-\3-\1\2\3\4.lst/'`
+  date=`echo "$file" | sed -E 's/.*\/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{6}).*/\1-\2-\3-\1\2\3\4.html/'`
   cp $file "../data/$date"
   echo "$file $date"
 done
