367f53b73df4ee85b5d3105b9c0abc16ca0f0937
Ryan Albrecht <ryan@ryanalbrecht.ca>
2016-06-09 19:43:57 -0700

    Prevent downloading and asking for different dates

--- getFaces.py
+++ getFaces.py
@@ -28,7 +28,7 @@ class DataImporter(object):
     today = getToday()
     if self.args.date:
       today = self.args.date
-      self.args.download = false # can't set another date and download
+      self.args.download = False # can't set another date and download
 
     todayClean = re.sub(r'(\d{4}\-\d{2}\-\d{2}).*', r'\1', today)
     htmlFile = self.settings['cacheDir'] + today + '.html'
