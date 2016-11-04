cfc0244eb39d484afee38fd3abe0c6376c9bb618
Ryan Albrecht <ryan@ryanalbrecht.ca>
2016-11-03 19:41:15 -0700

    Make a helper for posting data to slack and choosing a channel

--- slack.py
+++ slack.py
@@ -1,28 +1,25 @@
 import json
 import urllib2
 
-def postGhostsToSlack(endpoint, names, channel=None):
-  data = {
-    'text': '\n'.join(['%s is a FreshGhost' % (name,) for name in names]),
-  }
+def postSlackData(endpoint, data, channel):
   if channel:
     data['channel'] = channel
 
   print(json.dumps(data))
   urllib2.urlopen(urllib2.Request(endpoint, json.dumps(data)))
 
+def postGhostsToSlack(endpoint, names, channel=None):
+  postSlackData({
+    'text': '\n'.join(['%s is a FreshGhost' % (name,) for name in names]),
+  })
+
 def postFreshiesToSlack(endpoint, names, channel=None):
-  data = {
+  postSlackData({
     'text': '\n'.join(['%s is new\n' % (name,) for name in names]),
-  }
-
-  if channel:
-    data['channel'] = channel
-
-  urllib2.urlopen(urllib2.Request(endpoint, json.dumps(data)))
+  })
 
 def postGraphToSlack(endpoint, date, graphURL, summaryData, channel=None):
-  data = {
+  postSlackData({
     'attachments': [{
       'fallback': 'Today\'s employee chart is ready',
       'title': 'Employee Growth Chart - %s' % (date, ),
@@ -34,12 +31,4 @@ def postGraphToSlack(endpoint, date, graphURL, summaryData, channel=None):
       ],
       'image_url': graphURL,
     }]
-  }
-
-  if channel:
-    data['channel'] = channel
-
-  print(json.dumps(data))
-  print(urllib2.urlopen(
-    urllib2.Request(endpoint, json.dumps(data))
-  ))
+  })
