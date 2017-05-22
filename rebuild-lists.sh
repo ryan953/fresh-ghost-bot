d39e557c88eb2a9f8af0377935e513f530e0f3ad
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-05-21 19:53:28 -0700

    Make rebuild-lists point to the source file again

--- rebuild-lists.sh
+++ rebuild-lists.sh
@@ -4,7 +4,7 @@ source ./env/bin/activate
 
 for old in ./data/html_cache/*.html; do
   new=$(echo $old | sed -E 's/.*\/(.*).html/\1/')
-  python getFaces.py --save --date $new
+  python src/getFaces.py --save --date $new
 done
 
 deactivate
